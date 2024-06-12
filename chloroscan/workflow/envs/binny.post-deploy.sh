1464af6368c8806af366cdc324a04e597ef5bbc2
Yuhao Tong <yuhtong@spartan-bm083.hpc.unimelb.edu.au>
2024-06-12 19:16:02 +1000

    20240612 env recovery

--- chloroscan/workflow/envs/binny.post-deploy.sh
+++ chloroscan/workflow/envs/binny.post-deploy.sh
@@ -22,7 +22,7 @@ sed -i "s|checkm_filtered_tf_weight=0.5||g" $CFG_PATH
 sed -i '/^$/d' $CFG_PATH 
 # config sorted, now should change the database. Change the hmm profile for pf only.
 
-REPLACEMENT_HMM=$CHLOROSCAN_DIR/binny_algal_database/checkm_filtered_pf.hmm
+REPLACEMENT_HMM=$BINNY_DIR/binny_algal_database/checkm_filtered_pf.hmm
 
 CHECKM_PF_DIR="database/hmms/checkm_pf"
 rm ./database/hmms/checkm_pf/checkm_filtered_pf.hmm 
@@ -32,7 +32,7 @@ hmmpress -f $CHECKM_PF_DIR/checkm_filtered_pf.hmm
 rm -rf $CHECKM_PF_DIR/chunks/*
 cp $CHECKM_PF_DIR/checkm_filtered_pf.hmm $CHECKM_PF_DIR/chunks/checkm_filtered_pf_chunk_0.hmm && hmmpress -f $CHECKM_PF_DIR/chunks/checkm_filtered_pf_chunk_0.hmm
 
-REPLACEMENT_TSV=$CHLOROSCAN_DIR/binny_algal_database/taxon_marker_sets_lineage_sorted.tsv
+REPLACEMENT_TSV=$BINNY_DIR/binny_algal_database/taxon_marker_sets_lineage_sorted.tsv
 cp $REPLACEMENT_TSV ./database
 
 # export PATH=$BINNY_DIR:$PATH
