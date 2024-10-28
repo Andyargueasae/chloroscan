2f3ba3dea7b9d519cc34b1991360daa8e14eefa9
Yuhao Tong <yuhtong@spartan-login3.hpc.unimelb.edu.au>
2024-10-28 17:36:16 +1100

    Update fasta file writing, by taking sequence format into account

--- chloroscan/workflow/scripts/corgi-fasta_filter.py
+++ chloroscan/workflow/scripts/corgi-fasta_filter.py
@@ -1,11 +1,14 @@
 import os
 import click
 
+CONTIG_IDENTIFER = ">"
+
 @click.command()
 
 @click.option("--assembly", type=click.Path(exists=True))
 @click.option("--accession", type=click.Path(exists=True))
 @click.option("--fasta_file_name", type=click.STRING)
+
 
 def main(assembly, accession, fasta_file_name):
     plastid_accession_id = []
@@ -16,6 +19,7 @@ def main(assembly, accession, fasta_file_name):
             plastid_accession_id.append(plastid_accession)
     # print(len(plastid_accession_id)) 
 
+    n_contig = len(plastid_accession_id)
     with open(assembly, "r") as assemblyf:
         assembly_lines = assemblyf.readlines()
 
@@ -24,25 +28,40 @@ def main(assembly, accession, fasta_file_name):
     # make a judgement, whether there are files already there.
     if os.path.exists(fasta_file_name):
         os.remove(fasta_file_name)
-    CONTIG_IDENTIFER = ">"
+    
     index = 0
-
+    n_count = 0
+    # Here, adjust the code to jump across different contig accession line, and store the sequences under the desired accession ID.
     while index < len(assembly_lines):
+        if n_count == n_contig:
+            break
+        # By default, the first line is always an accession for the first sequence.
         accession_id = assembly_lines[index].replace(">","").split(" ")[0].strip()
-        # print(accession_id)
+        # Now, find the next index id. 
+        next_accession_index = get_next_accession(assembly_lines, index)
         if accession_id in plastid_accession_id:
-            # print("sequence classified as plastidial.\n")
-            sequence = assembly_lines[index+1]
-            # print(sequence)
+            sequence = "".join([seq.strip() for seq in assembly_lines[index+1:index+next_accession_index]])+"\n"
             with open(fasta_file_name, "a") as fasta:
                 fasta.write(CONTIG_IDENTIFER)
                 fasta.write(accession_id)
                 fasta.write("\n")
                 fasta.write(sequence)
-        index += 2
+                n_count += 1
+        index += next_accession_index
     
     # print(os.path.exists(fasta_file_name))
     return
+
+def get_next_accession(assembly_lines, index):
+    line_skip = 0
+    for i in range(index+1, len(assembly_lines)):
+        if CONTIG_IDENTIFER in assembly_lines[i]:
+            line_skip = i - index
+            return line_skip
+        else: # Make sure the sequence is already at the end.
+            line_skip = len(assembly_lines)-index
+
+    return line_skip
 
 if __name__ == "__main__":
     main()
