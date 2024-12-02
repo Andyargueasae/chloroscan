201d6be62119f88e1e4a29580d95dc81806825dc
Yuhao Tong <yuhtong@rs-l-dvd53w.novalocal>
2024-12-02 21:16:23 +1100

    pytest timestamping and testing finalizations

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -2,6 +2,6 @@ def test_binny(run_workflow):
     w = run_workflow("TEST_OUT/working/CAT")
     w.assert_exists()
     w.assert_dir_exists("TEST_OUT/working/CAT")
-    w.assert_include("out.CAT.contig2classification.txt")
+    w.assert_exists("TEST_OUT/working/CAT/out.CAT.contig2classification.txt")
     # w.assert_include("bins/*.fasta")
     # Perhaps add new outputs to the rule and test whether there are. 
--- tests/test_visualizing.py
+++ tests/test_visualizing.py
@@ -1,3 +1,6 @@
 def test_visualizing(run_workflow):
     w = run_workflow("TEST_OUT/working/visualizations")
     w.assert_exists()
+    w.assert_exists("TEST_OUT/working/visualizations/Scatter_GCLogDepth.png")
+    w.assert_exists("TEST_OUT/working/visualizations/LogDepth_Violin.png")
+    w.assert_exists("TEST_OUT/working/visualizations/TEST_OUT_binny_I04R01.000000_C84_P97_Haptophyta_taxonomy_composition.png")
