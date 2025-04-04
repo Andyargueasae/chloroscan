1947ed820bd2fe014579be187705dfa6582cbade
Yuhao Tong <yuhtong@spartan-login1.hpc.unimelb.edu.au>
2025-04-04 13:11:38 +1100

    simplify prediction of genes and proteins in using FragGeneScanRs alone

--- chloroscan/workflow/scripts/cds-extraction.sh
+++ chloroscan/workflow/scripts/cds-extraction.sh
@@ -12,33 +12,25 @@ FGSR_OUTPUT=${snakemake_params[path_fraggenescanrs]}
 CDS_OUTPUT=${snakemake_output[CDS_EXTRACTION]}
 
 mkdir -p $FGSR_OUTPUT/$FILE_FLAG
+mkdir -p $CDS_OUTPUT/cds
+mkdir -p $CDS_OUTPUT/faa
+echo ${snakemake_output}
 
-# iterate over each bin recovered by binny.
+
 for i in $(ls $BINNY_DIR);
 do 
     echo $BINNY_DIR/$i
     # echo ${snakemake_output}
-    FragGeneScanRs --seq-file-name $BINNY_DIR/$i --training-file illumina_5 --thread-num 1 -g $FGSR_OUTPUT/$FILE_FLAG/$(basename $i .fasta).gff -w 0; 
+    FragGeneScanRs --seq-file-name $BINNY_DIR/$i --training-file illumina_5 --thread-num 1 -g $FGSR_OUTPUT/$FILE_FLAG/$(basename $i .fasta).gff \
+     -n $CDS_OUTPUT/cds/$(basename $BATCH_NAME).$(basename $i .fasta).gene.fasta -a $CDS_OUTPUT/faa/$(basename $BATCH_NAME).$(basename $i .fasta).faa -w 0; 
 done
-
-# rm $FGSR_OUTPUT/$FILE_FLAG/*.fasta.fai.gff
-# rm $BINNY_DIR/$PATH_NAME_BINS/*.fai
-# Don't forget the end step, change the permission of all gff outputs, as if you don't make the change they will not be able to opened by gffread.
-# This script could make use of snakemake parameters to give cds extraction for those MAGs.
 
 # make the directory first.
-mkdir -p $CDS_OUTPUT/cds
-echo ${snakemake_output}
 
-for i in $(ls $FGSR_OUTPUT/$FILE_FLAG);
-do
-    echo $i
-    # gffread -C ${snakemake_input}/$i -o $(basename $i .gff).cds 2> ${snakemake_log};
-    gffread -w $(basename $BATCH_NAME).$(basename $i .gff).cds.fasta -g $BINNY_DIR/$(basename $i .gff).fasta $FGSR_OUTPUT/$FILE_FLAG/$i
-    mv $(basename $BATCH_NAME).$(basename $i .gff).cds.fasta $CDS_OUTPUT/cds
-    rm $BINNY_DIR/*.fai
-
-done
-# So basically, every script uses a for loop to do batch jobs for each of the files.
-## As this rule is the last rule of the workflow, touch one final file within output.
-# We can't just let the flag file come out, we must need to test whether there are files in cds here!
+# for i in $(ls $FGSR_OUTPUT/$FILE_FLAG);
+# do
+#     echo $i
+#     # gffread -C ${snakemake_input}/$i -o $(basename $i .gff).cds 2> ${snakemake_log};
+#     gffread -w $(basename $BATCH_NAME).$(basename $i .gff).cds.fasta -g $BINNY_DIR/$(basename $i .gff).fasta $FGSR_OUTPUT/$FILE_FLAG/$i
+#     mv $(basename $BATCH_NAME).$(basename $i .gff).cds.fasta $CDS_OUTPUT/cds
+#     rm $BINNY_DIR/*.fai
