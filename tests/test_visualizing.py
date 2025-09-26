84577e0880f29afc530d5818dd1e9ea38f892186
Yuhao Tong <yuhtong@spartan-login1.hpc.unimelb.edu.au>
2025-09-26 21:37:03 +1000

    chloroscan update test

--- tests/test_visualizing.py
+++ tests/test_visualizing.py
@@ -1,6 +1,7 @@
+import os
 def test_visualizing(run_workflow):
     w = run_workflow("TEST_OUT/working/visualizations")
     w.assert_exists()
     w.assert_exists("TEST_OUT/working/visualizations/Scatter_GCLogDepth.png")
     w.assert_exists("TEST_OUT/working/visualizations/LogDepth_Violin.png")
-    # w.assert_exists("TEST_OUT/working/visualizations/*_Haptophyta_taxonomy_composition.png")
+    assert len([figure for figure in os.listdir("TEST_OUT/working/visualizations") if figure.endswith("_taxonomy_composition.png")]) == 1, "There should be exactly one visualization output directory"
