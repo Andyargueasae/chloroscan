0fc2ca85cba69fdab41985a0d89b21d511a5ba49
Yuhao Tong <yuhtong@spartan-login2.hpc.unimelb.edu.au>
2026-03-10 16:00:54 +1100

    confpy wrong

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,5 +60,5 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
