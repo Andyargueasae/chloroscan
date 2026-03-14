64089655728d9f0456fec06cc553a84514057500
Yuhao Tong <yuhtong@spartan-login1.hpc.unimelb.edu.au>
2026-03-14 22:58:52 +1100

    update the binning testing

--- tests/test_2_binny.py
+++ tests/test_2_binny.py
@@ -2,7 +2,7 @@
 # Only you might fail, what is the exact reason?
 
 def test_binny(run_workflow):
-    w = run_workflow("TEST_OUT/working/binny")
+    w = run_workflow("TEST_OUT/working/binny/binny.done")
     w.assert_exists()
     w.assert_dir_exists("TEST_OUT/working/binny/bins")
     # w.assert_include("bins/*.fasta")
